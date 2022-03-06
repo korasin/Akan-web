1d49f93ae436b38347ea40b0a7a4e4954a27887e
korasin <mchemutai232@gmail.com>
2022-03-06 11:36:42 +0300

    Add function

--- akan.js
+++ akan.js
@@ -1,5 +1,3 @@
-
-var date=(1-31);
 function getDate(){
    console.log("Enter date");
    return ("Enter date");
@@ -8,5 +6,24 @@ function getDate(){
 }else if(date==32){
    console.log("invalid, enter another")
 }
-// var date=(1-31);
+var date=(1-31);
 getDate(0);
+function myday(dayAsNumber){
+   if (day==0){
+      console.log("sunday")
+   }else if (day==1){
+      console.log("monday")
+   }else if(day==2){
+      console.log("tuesday")
+   }else if(day==3){
+      console.log("wednesday")
+   }
+}
+let gender = new gender().getGender();
+let gender;
+  if (gender < male) {
+    gender = "male";
+  } else {
+    gender = "female";
+  }
+document.getElementById("demo").innerHTML = gender;
