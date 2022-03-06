5b2eac1311c822c9b38449ae05283cf4c22d29bf
korasin <mchemutai232@gmail.com>
2022-03-06 15:53:03 +0300

    Add changes to js and html

--- akan.js
+++ akan.js
@@ -1,11 +1,11 @@
-// function getDate(){
-//    console.log("date entered");
-//    return ("date entered");
-// }if (date==0){
-//    console.log("invalid, enter another")
-// }else if(date==32){
-//    console.log("invalid, enter another")
-// }
+function getDate(){
+   console.log("date entered");
+   return ("date entered");
+}if (date==0){
+   console.log("invalid, enter another")
+}else if(date==32){
+   console.log("invalid, enter another")
+}
 let myAge=(myAgeVar, myGenderVar)
 let age= `my Age is `(myAgevar, myGenderVar)
 
