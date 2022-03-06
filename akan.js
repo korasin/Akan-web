85290949da157d0fc2e005291edfd89de657290a
korasin <mchemutai232@gmail.com>
2022-03-06 16:18:45 +0300

    Add day function

--- akan.js
+++ akan.js
@@ -6,6 +6,22 @@ function getDate(){
 }else if(date==32){
    console.log("invalid, enter another")
 }
-let myAge=(myAgeVar, myGenderVar)
-let age= `my Age is `(myAgevar, myGenderVar)
+function myDay(){
+    console.log ("your name is" + date);
+}if (day==0){
+    console.log("sunday")
+}else if(day==1){
+    console.log("monday")
+}else if(day==2){
+    console.log("tuesday")
+}else if(day==3){
+    console.log("wednesday")
+}else if(day==4){
+    console.log("thursday")
+}else if(day==5){
+    console.log("friday")
+}else (day==6);{
+    console.log("saturday")
+}
+myDay();
 
